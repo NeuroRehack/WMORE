9d94006688c7aeac72a0957b17e7f31a20f67bd4
Ben Du <benjamin.du@uq.net.au>
2025-11-04 09:40:49 +1000

    Update zmodem_rz _v2.10_draft.cpp
    Set fout to SdFile type

--- Firmware/WMORE_Openlog_Logger/zmodem_rz _v2.10_draft.cpp
+++ Firmware/WMORE_Openlog_Logger/zmodem_rz _v2.10_draft.cpp
@@ -133,15 +133,7 @@ _PROTOTYPE(void bttyout , (int c ));
 #ifndef ARDUINO
 FILE *fout;
 #else
-#if SD_FAT_TYPE == 1
-extern File32 fout;
-#elif SD_FAT_TYPE == 2
-extern ExFile fout;
-#elif SD_FAT_TYPE == 3
-extern FsFile fout;
-#else // SD_FAT_TYPE == 0
-extern File fout;
-#endif  // SD_FAT_TYPE
+extern SdFile fout;
 #endif
 
 // Dylan (monte_carlo_ecm, bitflipper, etc.) - Moved this to a global variable to enable
